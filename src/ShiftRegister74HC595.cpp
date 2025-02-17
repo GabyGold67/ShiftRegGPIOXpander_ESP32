c4c1753df45af55510453f37dc71490b56915802
Gaby Goldman <82763943+GabyGold67@users.noreply.github.com>
2025-02-17 19:25:23 +0200

    Code update 2025-02-17b

--- src/ShiftRegister74HC595.cpp
+++ src/ShiftRegister74HC595.cpp
@@ -10,8 +10,8 @@ ShiftRegister74HC595::ShiftRegister74HC595(uint8_t serialDataPin, uint8_t clockP
    pinMode(_clockPin, OUTPUT);
    pinMode(_serialDataPin, OUTPUT);
    pinMode(_latchPin, OUTPUT);
-   
-   _srArryBuffPtr = new uint8_t[_srQty];
+
+   _srArryBuffPtr = new uint8_t[_srQty]();
    for(uint8_t i{0}; i < _srQty; i++)
       *(_srArryBuffPtr + i) = 0x00;
    _maxPin = _srQty * 8 - 1;
@@ -23,30 +23,8 @@ uint8_t ShiftRegister74HC595::digitalRead(const uint8_t &pin){
    return (_srArryBuffPtr[pin / 8] >> (pin % 8)) & 0x01;
 }
 
-uint8_t* ShiftRegister74HC595::getArryBuffPtr(){
-
-   return _srArryBuffPtr; 
-}
-
 void ShiftRegister74HC595::digitalWrite(const uint8_t pin, const uint8_t value){
    digitalWriteBuff(pin, value);
-   updShftRgstrs();
-
-   return;
-}
-
-uint8_t ShiftRegister74HC595::getMaxPin(){
-   
-   return _maxPin;
-}
-
-uint8_t ShiftRegister74HC595::getSrQty(){
-
-   return _srQty;
-}
-
-void ShiftRegister74HC595::digitalWriteOver(const uint8_t* digitalValues){
-   memcpy( _srArryBuffPtr, digitalValues, _srQty);   // dest, src, size
    updShftRgstrs();
 
    return;
@@ -69,18 +47,40 @@ void ShiftRegister74HC595::digitalWriteAllSet(){
 }
 
 void ShiftRegister74HC595::digitalWriteBuff(const uint8_t pin, const uint8_t value){
-    if(value)
-      *(_srArryBuffPtr + (pin/8)) |= (0x01 << (pin % 8));
-    else
-      *(_srArryBuffPtr + (pin/8)) &= ~(0x01 << (pin % 8));
+   if(value)
+     *(_srArryBuffPtr + (pin/8)) |= (0x01 << (pin % 8));
+   else
+     *(_srArryBuffPtr + (pin/8)) &= ~(0x01 << (pin % 8));
+
+  return;
+
+}
+
+void ShiftRegister74HC595::digitalWriteOver(const uint8_t* &newValues){
+   memcpy( _srArryBuffPtr, newValues, _srQty);   // dest, src, size
+   updShftRgstrs();
 
    return;
+}
 
+uint8_t* ShiftRegister74HC595::getArryBuffPtr(){
+
+   return _srArryBuffPtr; 
+}
+
+uint8_t ShiftRegister74HC595::getMaxPin(){
+   
+   return _maxPin;
+}
+
+uint8_t ShiftRegister74HC595::getSrQty(){
+
+   return _srQty;
 }
 
 void ShiftRegister74HC595::updShftRgstrs(){
     for (int i = _srQty - 1; i >= 0; i--) {
-        shiftOut(_serialDataPin, _clockPin, MSBFIRST, _srArryBuffPtr[i]);
+        shiftOut(_serialDataPin, _clockPin, MSBFIRST, *(_srArryBuffPtr + i));
     }    
     digitalWrite(_latchPin, HIGH); 
     digitalWrite(_latchPin, LOW); 
--- src/ShiftRegister74HC595.h
+++ src/ShiftRegister74HC595.h
@@ -5,16 +5,16 @@
   *
   * @details Additional information is available at https://timodenk.com/blog/shift-register-arduino-library/
   *
-  * @author	: Timo Denk and contributors
+  * @author	: Gabriel D. Goldman
   * 
   * @version v
-  * @date First release: 11/2014 
+  * @date First release: 12/02/2025 
   *       Last update:   12/02/2025 10:50 (GMT+0200)
   * 
   * @copyright Released into the public domain
   *
   ******************************************************************************
-  * @attention	This library was developed by Timo Denk and others for personal use only according to their needs.  * 
+  * @attention	This library was developed by Timo Denk and others for personal use only according to their needs.
   * @warning **Use of this library is under your own responsibility**
   ******************************************************************************
 */
@@ -44,70 +44,83 @@ public:
     * 
     * Instantiates a ShiftRegister74HC595 object, a model of a 74HCx595 -or more than one in daisy-chain connection- for an easy transparent bit or multibit state management.
     * 
-    * @param serialDataPin GPIO pin connected to the DS pin of the 74HC595 to send output data serially
+    * @param serialDataPin GPIO pin connected to the DS pin of the 74HC595 to send data serially to the shift register
     * @param clockPin GPIO pin connected to the SH_CP pin of the 74HC595 to manage the communication's clock
     * @param latchPin GPIO pin connected to the ST_CP pin of the 74HC595 to set (latch) the Q0~Q7 output pins from the internal buffer register.
-    * @param srQty Quantity of shift registers set in daisy-chain configuration (cascading)
+    * @param srQty Quantity of shift registers set in daisy-chain configuration.
     * 
-    * @attention The array of bytes set to hold the values to set to the shift registers will be arranged sequentially with a pointer to the first byte, meaning the pointer position +0 will hold bits 00~08, the pointer position +1 will hold bits 09~15 and so on, with the lower bit representing the LSb or rightmost bit
+    * @attention The array of bytes set to hold the values to set to the shift registers will be arranged sequentially with a pointer to the first byte, meaning the pointer position +0 will hold bits 00~07, the pointer position +1 will hold bits 08~15 and so on, with the lower bit representing the LSb or rightmost bit
     */
    ShiftRegister74HC595(uint8_t serialDataPin, uint8_t clockPin, uint8_t latchPin, uint8_t srQty = 1);
    /**
     * @brief Returns the state of the requested pin.
     * 
-    * @param pin Pin whose current value is required
+    * @param pin Pin whose current value is required.
+    * 
     * @return The state value of the requested pin, either HIGH (0x01) or LOW (0x00)
     * 
-    * @note The value is retrieved from the object's buffer, not the real chip
+    * @warning The value is retrieved from the object's buffer, not the real chip. If a change tothe buffer was made by using the digitalWriteBuff(const uint8_t, uint8_t) method without updating the shift register pins by using the updShftRgstrs() method, a difference might be expected.
     */
    uint8_t digitalRead(const uint8_t &pin);
    /**
-    * @brief Retrieves the states of the all the shift registers' output pins.
-    * 
-    * @return Pointer to the array of uint8_t holding the buffered shift registers values
-    *
-    * @note The returned array's length is equal to the number of shift registers set in daisy-chain.
-    */
-   uint8_t* getArryBuffPtr();
-   /**
    * @brief Set a specific pin to either HIGH (0x01) or LOW (0x00).
    * 
-   * @param pin a positive, zero-based integer, indicating which pin to set.
+   * @param pin a positive value indicating which pin to set. The valid range is 0 <= pin <= (srQty*8)-1
    * @param value Value to set for the indicated Pin
    */
-    void digitalWrite(const uint8_t pin, const uint8_t value);
-    uint8_t getMaxPin();
-    uint8_t getSrQty();
-    /**
-     * @brief Set all pins of the shift registers at once.
-     * 
-     * @param digitalValues uint8_t array where the length is equal to the number of shift registers.
-     */
-    void digitalWriteOver(const uint8_t * digitalValues);
-    /**
-     * @brief Sets all pins of all shift registers to HIGH (1).
-     */
-    void digitalWriteAllSet(); 
-    /**
-     * @brief Sets all pins of all shift registers to LOW (0).
-     * 
-     */
-    void digitalWriteAllReset();
+   void digitalWrite(const uint8_t pin, const uint8_t value);
    /**
-    * @brief Equivalent to set(int pin, uint8_t value), except the physical shift register is not updated.
+     * @brief Sets all pins of all shift registers to LOW (0x00).
+     */
+   void digitalWriteAllReset();
+   /**
+    * @brief Sets all pins of all shift registers to HIGH (0x01).
+    */
+   void digitalWriteAllSet(); 
+   /**
+    * @brief Equivalent to void digitalWrite(const uint8_t, const uint8_t), except the buffer is not pushed into the physical shift register.
     * 
-    * @param pin 
-    * @param value 
+    * @param pin Number of bit being modified in the object's buffer
+    * @param value The value to be set to the pin, either LOW (0x00) or HIGH (0x01)
     *
-    * @attention Should be used in combination with updShftRgstrs().
+    * @attention Should be used in combination with updShftRgstrs(), or the object's buffer an the shift register pins will be uncoordinated. Use with precaution: is usefull to avoid excesive uneeded communications with the shift register, but might generate inconsistencies.
    */
    void digitalWriteBuff(const uint8_t pin, uint8_t value);
    /**
-    * @brief Updates the shift register pins to the stored output values.
+     * @brief Overwrites the object buffer with new content
+     * 
+     * @param digitalValues uint8_t* pointer to an array with the new content to be set at the shift register/s.
+     * @note The method expects the pointer to be set to an array with the same lenght as srQty as it will be copied byte by byte over the object's buffer
+     */
+    void digitalWriteOver(const uint8_t* &newValues);
+   /**
+    * @brief Retrieves the pointer to the shift registers buffer.
+    * 
+    * @return Pointer to the array of uint8_t holding the buffered shift registers values
     *
-    * @note This is the function that actually writes data into the shift registers of the 74HC595.
+    * @note The returned array's length is equal to the number of shift registers set in daisy-chain, see uint8_t getSrQty() for information.
+    */
+   uint8_t* getArryBuffPtr();
+   /**
+     * @brief Return the greatest valid pin number, according to the quantity of shift registers connected
+     * 
+     * @return uint8_t maxPin value
+     */
+   uint8_t getMaxPin();
+   /**
+     * @brief Return the quantity of shift registers composing the object
+     * 
+     * The value is passed as a parameter in the class constructor
+     * 
+     * @return uint8_t The number of shift registers represented by the object
+     */
+   uint8_t getSrQty();
+   /**
+    * @brief Updates the shift register pins to the stored object's buffer values.
+    *
+    * @note This is the function that actually writes data into the 74HC595 shift registers.
    */
-  void updShftRgstrs();
+   void updShftRgstrs();
 };
 
 #endif //_SHIFTREGISTER74HC595_ESP32_H_
